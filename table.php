434d5523487814e9229467c71d1bbf0dc3c9e5fd
Jan-Erik Ilves <janilv@greeny.cs.tlu.ee>
2015-10-16 10:50:26 +0300

    new files

---
+++ table.php
@@ -0,0 +1,68 @@
+<?php
+	
+	// table.php
+	require_once("functions.php");
+	
+	//kas kasutaja tahab kustutada
+	// kas aadressireal on ?delete=??!??!?!
+	if(isset($_GET["delete"])){
+		
+		// saadan kaasa id, mida kustutada
+		deleteCar($_GET["delete"]);
+		
+	}
+	
+	
+	
+	$car_list = getCarData();
+	//var_dump($car_list);
+?>
+<table border=1 >
+	<tr>
+		<th>id</th>
+		<th>kasut id</th>
+		<th>auto nr märk</th>
+		<th>värv</th>
+		<th>X</th>
+	</tr>
+	
+	<?php
+	
+		// iga massiivis olema elemendi kohta
+		// count($car_list) - massiivi pikkus
+		for($i = 0; $i < count($car_list); $i++){
+			// $i = $i +1; sama mis $i += 1; sama mis $i++;
+			
+			// kui on see rida, mida kasutaja tahab muuta, siis kuvan input väljad
+			if(isset($_GET["edit"]) && $car_list[$i]->id == $_GET["edit"]){
+				//kasutajale muutmiseks
+				echo "<tr>";
+				echo "<form>";
+				echo "<td>".$car_list[$i]->id."</td>";
+				echo "<td>".$car_list[$i]->user_id."</td>";
+				echo "<td><input name='number plate' value='".$car_list[$i]->number_plate."'></td>";
+				echo "<td><input name='color' value='".$car_list[$i]->color."'></td>";
+				echo "<td><input type='submit' name='update'></td>";
+				echo "<td><a href='table.php'>cancel<a/></td>";
+				echo "</form>";
+				
+				echo "</tr>";
+			
+			}else{
+				//tavaline rida
+				echo "<tr>";
+				
+				echo "<td>".$car_list[$i]->id."</td>";
+				echo "<td>".$car_list[$i]->user_id."</td>";
+				echo "<td>".$car_list[$i]->number_plate."</td>";
+				echo "<td>".$car_list[$i]->color."</td>";
+				echo "<td><a href='?delete=".$car_list[$i]->id."'>X</a></td>";
+				echo "<td><a href='?edit=".$car_list[$i]->id."'>Edit</a></td>";
+				
+				echo "</tr>";
+			}
+		}
+	
+	?>
+
+</table>
